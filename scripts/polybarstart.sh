83697aa3ff612e9900af0ba135333830c6d5f47a
ali <mohsennbarzegar@gmail.com>
2019-05-05 15:30:46 +0430

    don't know dont't care

--- scripts/polybarstart.sh
+++ scripts/polybarstart.sh
@@ -8,4 +8,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 # polybar example &
-polybar example -r & 
+polybar example -r &
+polybar example2 -r &
--- scripts/tor.sh
+++ scripts/tor.sh
@@ -3,7 +3,7 @@
 status="$(curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs)"
 
 if [ ${#status} -gt 0 ]; then
-    echo "%{F#21ba42} Tor"
+    echo ""
 else
-    echo "%{F#ea0404} Tor"
+    echo "%{F#ea0404}"
 fi
