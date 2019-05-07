cc891afa9ff12e828f0f35701c24cba47eaf2ee6
ali <mohsennbarzegar@gmail.com>
2019-05-08 02:17:44 +0430

    added donations counter script (baraye farar az signal?)

---
+++ scripts/donations.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import requests
+
+def get_tor_session():
+    session = requests.session()
+    # Tor uses the 9050 port as the default socks port
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
+    return session
+
+# Make a request through the Tor connection
+session = get_tor_session()
+print(" " + session.get("https://phuck-donation.appspot.com/count").text)
